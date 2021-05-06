ed4b867777dd8735cf9549b20d3c83be5a91929b
Md. Hasan Sayeed <jbhasan@gmail.com>
2021-05-06 09:21:02 +0600

    Update middleware

--- src/Http/Middleware/JisfAuthenticate.php
+++ src/Http/Middleware/JisfAuthenticate.php
@@ -19,7 +19,7 @@ class JisfAuthenticate
 		$login_session = $request->session()->get('login');
 		$login_session = $login_session ? $login_session : null;
 		if (!$login_session || $login_session['status'] !== 'logged_in') {
-			$redirect_url = url()->previous();
+			$redirect_url = url()->current();
 			return redirect()->route('login', ['redirect' => $redirect_url]);
 		}
 
