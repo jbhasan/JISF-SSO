2b187e8d7e5eca3d54818e6a5492333bb75f37f0
Md. Hasan Sayeed <jbhasan@gmail.com>
2021-05-08 10:48:59 +0600

    Update Cookie

--- src/Http/Controllers/JisfSSOController.php
+++ src/Http/Controllers/JisfSSOController.php
@@ -3,6 +3,7 @@
 namespace Sayeed\JisfSSO\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Cookie\CookieJar;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,11 +18,9 @@ class JisfSSOController extends Controller
 		return redirect(config('jisf.login_sso_url') . '?referer=' . base64_encode(url($redirect_url)));
 	}
 	public function loginResponse(Request $request) {
-		$data = json_decode(base64_decode($request->data), true);
-
+		$data = json_decode(gzuncompress(base64_decode($request->data)), true);
 		if ($data['status'] == 'success' && !empty($data['user_info'])) {
-
-			setcookie('_ndoptor', $request->data, strtotime( '+1 days' ), '/');
+			$this->createNewCookie($data);
 			if ($request->has('redirect') && $request->get('redirect') != '') {
 				return redirect($request->get('redirect'));
 			} else {
@@ -30,6 +29,12 @@ class JisfSSOController extends Controller
 		} else {
 			return redirect('/login');
 		}
+	}
+
+	public function createNewCookie($requested_data_new) {
+		$requested_data_new = base64_encode(gzcompress(json_encode($requested_data_new)));
+		setcookie('_ndoptor', $requested_data_new, strtotime( '+1 days' ), '/');
+		return $requested_data_new;
 	}
 
 	public function logout(Request $request) {
--- src/Http/Middleware/JisfAuthenticate.php
+++ src/Http/Middleware/JisfAuthenticate.php
@@ -3,26 +3,32 @@
 namespace Sayeed\JisfSSO\Http\Middleware;
 
 use Closure;
+use Illuminate\Cookie\CookieJar;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cookie;
 
 class JisfAuthenticate
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
-    public function handle(Request $request, Closure $next)
-    {
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \Closure  $next
+	 * @return mixed
+	 */
+	public function handle(Request $request, Closure $next)
+	{
 		$login_cookie = isset($_COOKIE['_ndoptor']) ? $_COOKIE['_ndoptor'] : null;
-		$login_data_from_cookie = json_decode(base64_decode($login_cookie), true);
+		if (!$login_cookie) {
+			$redirect_url = url()->current();
+			return redirect()->route('login', ['redirect' => $redirect_url]);
+		}
+		$login_data_from_cookie = json_decode(gzuncompress(base64_decode($login_cookie)), true);
 		if (!$login_data_from_cookie || $login_data_from_cookie['status'] !== 'success') {
 			$redirect_url = url()->current();
 			return redirect()->route('login', ['redirect' => $redirect_url]);
 		}
 
-        return $next($request);
-    }
+		return $next($request);
+	}
 }
