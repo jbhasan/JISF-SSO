44392e68a6ab97f814794053cacd8e6aa45810af
Md. Hasan Sayeed <jbhasan@gmail.com>
2022-01-30 12:50:33 +0600

    Update Middleware

--- src/Http/Middleware/JisfAuthenticate.php
+++ src/Http/Middleware/JisfAuthenticate.php
@@ -20,12 +20,12 @@ class JisfAuthenticate
 	{
 		$login_cookie = isset($_COOKIE['_ndoptor']) ? $_COOKIE['_ndoptor'] : null;
 		if (!$login_cookie) {
-			$redirect_url = url()->current();
+			$redirect_url = url()->full();
 			return redirect()->route('login', ['redirect' => $redirect_url]);
 		}
 		$login_data_from_cookie = json_decode(gzuncompress(base64_decode($login_cookie)), true);
 		if (!$login_data_from_cookie || $login_data_from_cookie['status'] !== 'success') {
-			$redirect_url = url()->current();
+			$redirect_url = url()->full();
 			return redirect()->route('login', ['redirect' => $redirect_url]);
 		}
 
